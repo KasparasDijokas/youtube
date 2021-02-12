427d34f0feba17787e54d289e76a95b8cff56259
Kasparas Dijokas <dijokas5@gmail.com>
2021-02-12 03:16:42 +0200

    axios and design

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,21 +5,32 @@ import Videos from './components/Videos/Videos';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Trending from './pages/Trending';
 import Home from './pages/Home';
+import Watch from './pages/Watch';
+import React, { useState } from 'react';
+import YoutubeContext from './YoutubeContext';
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-          <Header />
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/trending" component={Trending} />
+  const [input, setInput] = useState('javascript');
+  const userSearchHandler = (userInput) => {
+    setInput(userInput);
+  };
 
-          <Sidebar />
-          <Videos />
-        </Switch>
-      </Router>
-    </div>
+  return (
+    <YoutubeContext.Provider value={{ keyword: input }}>
+      <div className="app">
+        <Router>
+          <Header userSearchHandler={userSearchHandler} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/trending" component={Trending} />
+            <Route exact path="/watch/:id" component={Watch} />
+
+            <Sidebar />
+            <Videos />
+          </Switch>
+        </Router>
+      </div>
+    </YoutubeContext.Provider>
   );
 }
 
